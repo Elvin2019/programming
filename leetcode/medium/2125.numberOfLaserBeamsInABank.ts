d16ff659572836f1c06d8163512f31f4e299a87b
elvin2019 <elvinahmadov0@gmail.com>
2024-01-09 00:49:06 +0400

    217. Contains Duplicate

--- leetcode/medium/2125.numberOfLaserBeamsInABank.ts
+++ leetcode/medium/2125.numberOfLaserBeamsInABank.ts
@@ -1,37 +1,24 @@
 function numberOfBeams(bank: string[]): number {
-    let current=0
-    let prev=0;
-    let res=0;
-  
-    for(const row of bank){
-        if(current){
-            prev = current;
-        }
-        current = 0;
-        for(const char of row){
-            if(char === '1'){
-                current++ ;
-            }
-        }
-        console.log(current, prev)
-      
-        if(prev && current){
-            // console.log(current, prev)
-            res += prev * current
-        }
-       
+  let current = 0;
+  let prev = 0;
+  let res = 0;
 
-        // if(!current){
-        //     //ignore
-        //     continue    
-        // }
-
+  for (const row of bank) {
+    if (current) {
+      prev = current;
     }
-    console.log(res)
-    return res;
-    
+    current = 0;
+    for (const char of row) {
+      if (char === "1") {
+        current++;
+      }
+    }
+
+    if (prev && current) {
+      res += prev * current;
+    }
+  }
+  return res;
 }
 
-
-
-numberOfBeams(["011001", "000000", "010100", "001000"])
+numberOfBeams(["011001", "000000", "010100", "001000"]);
