93c750e331ccaa7ceb1138ec0b33c447bd1db8c6
elvin2019 <elvinahmadov0@gmail.com>
2024-01-29 10:36:05 +0400

    new version

--- leetcode/easy/125.validPalindrome.ts
+++ leetcode/easy/125.validPalindrome.ts
@@ -4,7 +4,38 @@ function isPalindrome(s: string): boolean {
     return str === reverseStr
 }
 
-const IsPalindrome = isPalindrome("race a car");
+let IsPalindrome = isPalindrome("race a car");
 console.log(IsPalindrome);
 
 
+
+const isAlphaNumeric = (c: string, i: number) => {
+    const code = c.charCodeAt(i);
+    return (
+        (code > 47 && code < 58) || // numeric (0-9)
+        (code > 64 && code < 91) || // upper alpha (A-Z)
+        (code > 96 && code < 123) // lower alpha (a-z)
+    );
+}
+
+const isPalindrome_2 = (s: string): boolean => {
+    let left = 0
+    let right = s.length - 1
+    while (left < right) {
+        while (left < right && !isAlphaNumeric(s, left)) {
+            left++
+        }
+        while (left < right && !isAlphaNumeric(s, right)) {
+            right--
+        }
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+
+IsPalindrome = isPalindrome("race a car");
+console.log(IsPalindrome);
