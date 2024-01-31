52942f2cf5a7d0fc621181243234cd68f2894760
elvin2019 <elvinahmadov0@gmail.com>
2024-01-31 23:06:21 +0400

    remove extra logs

--- leetcode/medium/167.twoSum.ts
+++ leetcode/medium/167.twoSum.ts
@@ -3,8 +3,6 @@ function twoSum(numbers: number[], target: number): number[] {
     let right = numbers.length - 1;
 
     while (left < right) {
-        console.log(numbers[left], numbers[right])
-        
         if (numbers[left] + numbers[right] === target) {
             return [left + 1, right + 1];
         } else if (numbers[left] + numbers[right] < target) {
@@ -13,9 +11,7 @@ function twoSum(numbers: number[], target: number): number[] {
             right--;
         }
     }
-
     return [0, 0];
-    
 };
 
 const two = twoSum([2, 7, 11, 15], 9)
