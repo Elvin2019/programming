8d438a869a61312af843d239abdd90075f5a1cfd
elvin2019 <elvinahmadov0@gmail.com>
2024-02-01 19:47:39 +0400

    cleaning and explaining

--- leetcode/medium/015.3sum.ts
+++ leetcode/medium/015.3sum.ts
@@ -1,9 +1,4 @@
 function threeSum(nums: number[]): number[][] {
-    
-
-
-    // 
-
     let left = 0;
     let right = 0;
     nums.sort((a, b) => a - b);
@@ -11,32 +6,29 @@ function threeSum(nums: number[]): number[][] {
 
     for (let i = 0; i < nums.length - 2; i++) {
 
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicates
 
         left = i + 1;
         right = nums.length - 1;
 
         while (left < right) {
-            
-            if (nums[i] + nums[left] + nums[right] === 0) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if(sum > 0){
+                right--;
+            }
+            else if(sum < 0){
+                left++;
+            }
+            else{
                 result.push([nums[i], nums[left], nums[right]]);
                 left++;
                 right--;
                 while (left < right && nums[left] === nums[left - 1]) left++;
                 while (left < right && nums[right] === nums[right + 1]) right--;
-            } else if (nums[i] + nums[left] + nums[right] > 0) {
-                right--;
-            } else {
-                left++;
             }
         }
     }
-
-    console.log(result)
     return result
-
-    
 };
-
 
 threeSum([-1, 0, 1, 2, -1, -4])
