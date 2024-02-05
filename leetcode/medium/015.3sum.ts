bcbfef280c53f785dbad979fe3aa4835b1ffa87e
elvin2019 <elvinahmadov0@gmail.com>
2024-02-05 17:58:56 +0400

    cleaning

--- leetcode/medium/015.3sum.ts
+++ leetcode/medium/015.3sum.ts
@@ -22,9 +22,7 @@ function threeSum(nums: number[]): number[][] {
             else{
                 result.push([nums[i], nums[left], nums[right]]);
                 left++;
-                right--;
-                while (left < right && nums[left] === nums[left - 1]) left++;
-                while (left < right && nums[right] === nums[right + 1]) right--;
+                while (left < right && nums[left] === nums[left - 1]) left++; // skip duplicates
             }
         }
     }
