5e0bbd02c2a65c7a0993ef94b51e6fb4ed896484
elvin2019 <elvinahmadov0@gmail.com>
2024-02-08 18:59:16 +0400

    remove logs

--- leetcode/easy/704.binarySearch.ts
+++ leetcode/easy/704.binarySearch.ts
@@ -1,13 +1,11 @@
 function search(nums: number[], target: number): number {
      let l = 0, r = nums.length - 1;
      while(l<=r){
+        console.log(l, r)
         let m = Math.floor((l + r)/2);
         if(nums[m] === target) return m;
         else if(nums[m] < target) l = m + 1;
         else r = m - 1;
      }
-
      return -1;
-
 };
-console.log(search([1,3,5,6], 5))
