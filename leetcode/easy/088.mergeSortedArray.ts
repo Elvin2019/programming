fffc97bc91b53777b196b84f72b5811d1f1260ae
elvin2019 <elvinahmadov0@gmail.com>
2024-12-13 15:16:52 +0400

    tscofig is added

---
+++ leetcode/easy/088.mergeSortedArray.ts
@@ -0,0 +1,7 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    const mergeResult = nums1.slice(0, m)
+    console.log(mergeResult)
+ };
