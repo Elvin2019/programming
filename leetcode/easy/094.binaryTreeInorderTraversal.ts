f05c546fda6b80165941754f2fde2d87d6b414cf
elvin2019 <elvinahmadov0@gmail.com>
2024-12-20 16:35:59 +0400

    94. Binary Tree Inorder Traversal

---
+++ leetcode/easy/094.binaryTreeInorderTraversal.ts
@@ -0,0 +1,35 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  if (root === null) return [];
+  return [
+    ...inorderTraversal(root.left),
+    root.val,
+    ...inorderTraversal(root.right),
+  ];
+}
+
+console.log(
+  inorderTraversal({
+    val: 1,
+    left: null,
+    right: {
+      val: 2,
+      left: {
+        val: 3,
+        left: null,
+        right: null,
+      },
+      right: null,
+    },
+  })
+)
