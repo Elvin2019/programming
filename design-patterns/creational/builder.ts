656b5be73feb0caeb0d427912db81e42fa7dcd2b
elvin2019 <elvinahmadov0@gmail.com>
2025-01-08 11:47:55 +0400

    Design patterns creational abstract builder

---
+++ design-patterns/creational/builder.ts
@@ -0,0 +1,42 @@
+/**
+ * Separates the construction of a complex object from its representation, allowing the same construction process to create different representations.
+
+Example: Building a complex document.
+ */
+
+class House {
+    windows: number = 0;
+    doors: number = 0;
+    roof: boolean = false;
+  }
+  
+  class HouseBuilder {
+    private house: House = new House();
+  
+    setWindows(count: number): this {
+      this.house.windows = count;
+      return this;
+    }
+  
+    setDoors(count: number): this {
+      this.house.doors = count;
+      return this;
+    }
+  
+    setRoof(hasRoof: boolean): this {
+      this.house.roof = hasRoof;
+      return this;
+    }
+  
+    build(): House {
+      return this.house;
+    }
+  }
+  
+  const house = new HouseBuilder()
+    .setWindows(4)
+    .setDoors(2)
+    .setRoof(true)
+    .build();
+  console.log(house);
+  
